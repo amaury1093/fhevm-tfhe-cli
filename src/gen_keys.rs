89b0737bdf260061c6140178045b67e4bc77a5cd
Amaury <1293565+amaury1729@users.noreply.github.com>
2024-02-07 11:31:26 +0100

    chore(tfhe-rs): update to tfhe-rs 0.5.1

--- src/gen_keys.rs
+++ src/gen_keys.rs
@@ -3,15 +3,10 @@
 // Copyright © 2023 ZAMA.
 // All rights reserved.
 
-use tfhe::{
-    generate_keys, shortint::parameters::PARAM_MESSAGE_2_CARRY_2_KS_PBS, ClientKey,
-    CompactPublicKey, ConfigBuilder, ServerKey,
-};
+use tfhe::{generate_keys, ClientKey, CompactPublicKey, ConfigBuilder, ServerKey};
 
 pub fn gen_keys() -> (ClientKey, ServerKey, CompactPublicKey) {
-    let config = ConfigBuilder::all_disabled()
-        .enable_custom_integers(PARAM_MESSAGE_2_CARRY_2_KS_PBS, None)
-        .build();
+    let config = ConfigBuilder::default().build();
     let (cks, sks) = generate_keys(config);
     let pks = CompactPublicKey::new(&cks);
     (cks, sks, pks)
